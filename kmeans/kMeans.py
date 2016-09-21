961432cafd09d44d8617d591dc362c2bc6cc0a87
tonye0115 <tonye0115>
2016-09-21 09:44:10 +0800

    k-均值聚类算法

--- kmeans/kMeans.py
+++ kmeans/kMeans.py
@@ -42,6 +42,14 @@ def randCent(dataSet, k):
     return centroids
 
 def kMeans(dataSet, k, distMeas=disEclud, createCent=randCent):
+    '''
+    k-均值聚类算法
+    :param dataSet:
+    :param k:
+    :param distMeas:
+    :param createCent:
+    :return:
+    '''
     m = shape(dataSet)[0]
     clusterAssment = mat(zeros((m , 2)))
     centroids = createCent(dataSet, k)
@@ -72,8 +80,54 @@ def kMeans(dataSet, k, distMeas=disEclud, createCent=randCent):
             centroids[cent, :] = mean(ptsInClust, axis=0)  # 按列放心计算平均值
     return centroids, clusterAssment
 
+
+def biKmeans(dataSet, k, distMeas=disEclud):
+    '''
+    二分k-均值聚类算法
+    :param dataSet:
+    :param k:
+    :param distMeas:
+    :return:
+    '''
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m, 2)))
+
+    #创建初始簇
+    centroid0 = mean(dataSet, axis=0).tolist()[0]
+    centList = [centroid0]
+    for j in range(m):
+        clusterAssment[j, 1] = distMeas(mat(centroid0), dataSet[j, :])**2
+    while (len(centList) < k):
+        lowestSSE = inf
+        for i in range(len(centList)):
+            # 尝试划分每一簇
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:, 0].A == i)[0], :]
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = sum(splitClustAss[:, 1])
+            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:, 0].A != i)[0], 1])
+            print "sseSplit, and notSplit: ", sseSplit, sseNotSplit
+            if(sseSplit + sseNotSplit) < lowestSSE:  # 如果划分的SSE值最小， 则本次划分被保存
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        # 更新簇的分配结果
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+
+        print 'the bestCentToSplit is: ', bestCentToSplit
+        print 'the len of bestClustAss is: ', len(bestClustAss)
+
+        centList[bestCentToSplit] = bestNewCents[0, :]
+        centList.append(bestNewCents[1, :])
+
+        clusterAssment[nonzero(clusterAssment[:, 0].A == bestCentToSplit)[0], :] = bestClustAss
+
+        print centList
+
+
 if __name__ == '__main__':
-   dataMat = loadDateSet('testSet.txt')
+   dataMat = loadDateSet('testSet2.txt')
    '''
    print dataMat
 
@@ -87,4 +141,6 @@ if __name__ == '__main__':
 
    #print disEclud(dataMat[0], dataMat[1])
 
-   print kMeans(dataMat, 4)
+   #print kMeans(dataMat, 4)
+
+   biKmeans(dataMat, 3)
